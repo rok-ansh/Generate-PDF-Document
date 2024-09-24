143529c8326c0f03aa398dea75f2b47bd8240108
rohit <rohit26av@gmail.com>
2024-09-24 08:57:54 +0530

    Adding footer page to master page as well as sub page

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='P',unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv('topics.csv')
 
@@ -12,14 +13,27 @@ df = pd.read_csv('topics.csv')
 # pdf.cell(w=0, h=12,txt="Hello Pune", align='C', ln=1, border=1)
 
 for index, row in df.iterrows():
+    # Set header for master page
     pdf.add_page()
     pdf.set_font(family='Times', style='B', size=24)
-    pdf.set_text_color(254,0,0)
+    pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12,txt=row["Topic"], align='L', ln=1)
     pdf.line(10,22,200, 22)
 
+    # Set footer for master page
+    pdf.ln(265)
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.set_text_color(150, 150, 150)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align='R', ln=1)
+
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+        # Set footer for subpage
+        pdf.ln(277)
+        pdf.set_font(family='Times', style='I', size=8)
+        pdf.set_text_color(150, 150, 150)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align='R', ln=1)
 
 # Create multiple pdf
 pdf.output("output.pdf" "")
+print("Done")
