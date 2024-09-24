8c607c290f2dfe59fa7abecbca656911ddc44fb2
rohit <rohit26av@gmail.com>
2024-09-24 08:29:55 +0530

    Initial Commit adding name of odf ab=nd creating a page for each variable

---
+++ main.py
@@ -0,0 +1,23 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation='P',unit='mm', format='A4')
+
+df = pd.read_csv('topics.csv')
+
+# To create a single page
+# pdf.add_page()
+# pdf.set_font(family='Times', style='B', size=12)
+# pdf.cell(w=5, h=12,txt="Hello Mumbai", align='C', ln=1, border=1)
+# pdf.cell(w=0, h=12,txt="Hello Pune", align='C', ln=1, border=1)
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family='Times', style='B', size=24)
+    pdf.set_text_color(254,0,0)
+    pdf.cell(w=0, h=12,txt=row["Topic"], align='L', ln=1)
+    pdf.line(10,22,200, 22)
+
+# Create multiple pdf
+pdf.output("output.pdf" "")
+print("Done")
