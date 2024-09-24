2c3cfd52a7e75d941ca046247bfef2a3f7ee04dc
rohit <rohit26av@gmail.com>
2024-09-24 09:07:42 +0530

    Adding lines to the pages

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ for index, row in df.iterrows():
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12,txt=row["Topic"], align='L', ln=1)
-    pdf.line(10,22,200, 22)
+    # Adding line to master page
+    for i in range(0, 270, 10):
+        pdf.line(10,22+i,200, 22+i)
 
     # Set footer for master page
     pdf.ln(265)
@@ -33,6 +35,10 @@ for index, row in df.iterrows():
         pdf.set_font(family='Times', style='I', size=8)
         pdf.set_text_color(150, 150, 150)
         pdf.cell(w=0, h=12, txt=row["Topic"], align='R', ln=1)
+
+        # Adding line to subpages
+        for i in range(0, 290, 10):
+            pdf.line(10, 10 + i, 200, 10 + i)
 
 # Create multiple pdf
 pdf.output("output.pdf" "")
